90293634f1badc620e1302f94f6f75261d5dcf8d
mobber007 <thefactsmob@gmail.com>
2018-04-05 18:00:25 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@
       return {
         error: '',
         loadingOn: true,
-        loadingMessage: 'Loading WP Vue',
+        loadingMessage: 'Loading Vuepress',
         loadingWheel: true,
         showUpdater: false,
         viewKey: 0,
