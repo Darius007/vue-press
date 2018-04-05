f2c75bdb8691e262ee24b914a103e714ea2d860b
mobber007 <thefactsmob@gmail.com>
2018-04-05 16:59:30 +0300

    Update Feed.vue

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -4,7 +4,7 @@
     <section>
       <p>
         WP Vue is a simple template built with Vue JS that displays posts from a WordPress REST API endpoint.
-        Take what you see here &amp; rip it apart to suit your needs. To improve it for everyone else, <a href="https://www.github.com/alexmacarthur/wp-vue">contribute on Github</a>.
+        Take what you see here &amp; rip it apart to suit your needs. To improve it for everyone else, <a href="https://www.github.com/mobber007/wp-vue">contribute on Github</a>.
       </p>
     </section>
 
